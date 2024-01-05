b33ce0af12982316735e19f6c26e9f30d58d9d0b
epike <epikeran@gmail.com>
2024-01-05 13:59:26 +0900

    feat: Prepare interceptor component for authorization

---
+++ api/src/main/java/com/jakedelivery/api/_core/interceptor/AuthorizationInterceptor.java
@@ -0,0 +1,35 @@
+package com.jakedelivery.api._core.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AuthorizationInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("Authorization Interceptor url : {}", request.getRequestURI());
+
+        // WEB, 특히 Chrome 의 경우 GET, POST 요청 전 메소드 지원하는지 OPTIONS = pass
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            return true;
+        }
+
+        // js. html. png resource 를 요청하는 경우 = pass
+        if (handler instanceof ResourceHttpRequestHandler) {
+            return true;
+        }
+
+        // TODO: header 검증
+
+        return true;
+    }
+}
