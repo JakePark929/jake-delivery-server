31e85013bea5afc39e211bf654b65e080c1b4fdb
epike <epikeran@gmail.com>
2024-01-10 15:08:37 +0900

    feat: Add consumer logic in admin server

---
+++ store-admin/src/main/java/com/jakedelivery/storeadmin/userorder/consumer/UserOrderConsumer.java
@@ -0,0 +1,17 @@
+package com.jakedelivery.storeadmin.userorder.consumer;
+
+import com.jakedelivery.message.model.UserOrderMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class UserOrderConsumer {
+    @RabbitListener(queues = "delivery.queue")
+    public void userOrderConsumer(UserOrderMessage userOrderMessage) {
+        log.info("message queue >> {}", userOrderMessage);
+    }
+}
