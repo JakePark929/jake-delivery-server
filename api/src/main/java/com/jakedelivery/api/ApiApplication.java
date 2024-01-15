009386a7f603ab719287d0b1dc9627081e7f6f07
epike <epikeran@gmail.com>
2024-01-15 17:27:33 +0900

    feat: Migrate main project to kotlin

--- api/src/main/java/com/jakedelivery/api/ApiApplication.java
+++ api/src/main/java/com/jakedelivery/api/ApiApplication.java
@@ -1,11 +1,10 @@
 package com.jakedelivery.api;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
 @SpringBootApplication
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
 }
+*/
