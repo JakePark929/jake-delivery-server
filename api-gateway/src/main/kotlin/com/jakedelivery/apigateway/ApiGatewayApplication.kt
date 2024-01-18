eaac412decb0121e773eb046bd4213097c665dfd
epike <epikeran@gmail.com>
2024-01-18 11:02:36 +0900

    feat: Set up spring cloud gateway

---
+++ api-gateway/src/main/kotlin/com/jakedelivery/apigateway/ApiGatewayApplication.kt
@@ -0,0 +1,12 @@
+package com.jakedelivery.apigateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGatewayApplication {
+}
+
+fun main(args: Array<String>) {
+    runApplication<ApiGatewayApplication>(*args)
+}
